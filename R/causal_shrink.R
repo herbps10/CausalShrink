0f4af186a3c405b07664e896a543cf37ffcb79b5
Herb Susmann <herbps10@gmail.com>
2024-12-10 10:16:47 -0500

    Updated hard thresholding

--- R/causal_shrink.R
+++ R/causal_shrink.R
@@ -1,20 +1,20 @@
 causal_shrink <- function(point_estimates, efficiency_bounds, sample_size, method = "l2", lambda_max = 2) {
   if(method == "l2") {
-    lambda <- (efficiency_bounds / sample_size) / (point_estimates^2)
+    lambda <- 1 / sample_size * (sum(efficiency_bounds) / sum(point_estimates^2))
     shrinkage <- (1 / (1 + lambda))
-
     point_estimates_star <- shrinkage * point_estimates
   }
   else if(method == "l1") {
     lambda <- soft_thresholded_optimal_lambda(point_estimates, sqrt(efficiency_bounds / sample_size), lambda_max)
-
     point_estimates_star <- soft_threshold(point_estimates, lambda)
   }
   else if(method == "l0") {
     lambda <- hard_thresholded_optimal_lambda(point_estimates, sqrt(efficiency_bounds / sample_size), lambda_max)
-
     point_estimates_star <- hard_threshold(point_estimates, lambda)
   }
 
-  point_estimates_star
+  list(
+    point_estimates = point_estimates_star,
+    lambda = lambda
+  )
 }
--- R/soft_threshold.R
+++ R/soft_threshold.R
@@ -9,16 +9,16 @@ soft_thresholded_normal_mean <- function(lambda, mu, sigma) {
 soft_thresholded_normal_variance <- function(lambda, mu, sigma) {
   2 * mu^2 + 2 * lambda^2 + 2 * sigma^2 -
     ((mu + lambda)^2 + sigma^2) * (1 - pnorm(-lambda, mu, sigma)) - (mu + lambda) * sigma^2 * dnorm(-lambda, mu, sigma) -
-    ((mu - lambda)^2 + sigma^2) * (pnorm(lambda, mu, sigma)) + (mu - lambda) * sigma^2 * dnorm(lambda, mu, sigma) - thresholded_normal_mean(lambda, mu, sigma)^2
+    ((mu - lambda)^2 + sigma^2) * (pnorm(lambda, mu, sigma)) + (mu - lambda) * sigma^2 * dnorm(lambda, mu, sigma) - soft_thresholded_normal_mean(lambda, mu, sigma)^2
 }
 
 soft_thresholded_mse <- Vectorize(function(lambda, mu, sigma) {
-  return((mu - soft_thresholded_normal_mean(lambda, mu, sigma))^2 + soft_thresholded_normal_variance(lambda, mu, sigma))
-})
+  return(sum((mu - soft_thresholded_normal_mean(lambda, mu, sigma))^2) + sum(soft_thresholded_normal_variance(lambda, mu, sigma)))
+}, vectorize.args = "lambda")
 
 soft_thresholded_optimal_lambda <- function(mu, sigma, lambda_max = 2) {
-  #optimize(\(lambda) mse(lambda, mu, sigma), c(0, lambda_max))$minimum
+  #optimize(\(lambda) soft_thresholded_mse(lambda, mu, sigma), c(0, lambda_max))$minimum
   lambdas <- seq(0, lambda_max, length.out = 1e3)
-  mses <- mse(lambdas, mu, sigma)
+  mses <- soft_thresholded_mse(lambdas, mu, sigma)
   lambdas[which.min(mses)]
 }
