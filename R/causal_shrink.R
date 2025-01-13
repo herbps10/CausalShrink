aa5833239423af5a875e6940c8d7c01c2b26b373
Herb Susmann <herbps10@gmail.com>
2025-01-12 19:54:12 -0500

    Updated empirical bayes method

--- R/causal_shrink.R
+++ R/causal_shrink.R
@@ -1,13 +1,17 @@
 causal_shrink <- function(point_estimates, efficiency_bounds, sample_size, method = "l2", type = "default", lambda_max = 2) {
   shrinkage <- rep(NA, length(point_estimates))
+  lambda <- rep(NA, length(point_estimates))
   if(method == "l2") {
     lambda <- 1 / sample_size * (sum(efficiency_bounds) / sum(point_estimates^2))
     shrinkage <- rep((1 / (1 + lambda)), length(point_estimates))
     point_estimates_star <- shrinkage * point_estimates
   }
   else if(method == "empirical_bayes") {
-    lambda <- (1 / sample_size) * efficiency_bounds / (1 / length(point_estimates - 1) * sum(point_estimates^2))
-    shrinkage <- 1 / (1 + lambda)
+    #lambda <- (1 / sample_size) * efficiency_bounds / (1 / length(point_estimates - 1) * sum(point_estimates^2))
+    #shrinkage <- 1 / (1 + lambda)
+    tau_sq <- 1 / (length(point_estimates) - 1) * sum(point_estimates^2)
+    sigma_sq <- 1 / sample_size * efficiency_bounds
+    shrinkage <- tau_sq / (sigma_sq + tau_sq)
     point_estimates_star <- shrinkage * point_estimates
   }
   else if(method == "l1") {
